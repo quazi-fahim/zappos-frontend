9015ebb5f64f55c20afeaaa5fca2e95d5515e7b5
Quazi fahim <fahimahmad.4388@gmail.com>
2025-03-29 12:08:57 +0530

    fix issue

--- src/Route/Allroute.jsx
+++ src/Route/Allroute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Component/Pages/Home'
 import Signin from '../Component/Pages/login/signin'
-import Register from '../Component/Pages/login/register'
+import Register from '../Component/Pages/login/Register'
 
 const Allroute = () => {
   return (
