20f4e65f59215a1eed8800163146d4854531140a
Quazi fahim <fahimahmad.4388@gmail.com>
2025-03-29 12:20:36 +0530

    fix issue

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port:  3000, // Render will use PORT else default to 3000
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    strictPort: true,
+    allowedHosts: ['zappos-frontend.onrender.com'] // 👈 Add your Render domain here
   }
 });
